a464430449373c135b8ee07ca0c5997e8965f72d
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2019-11-29 22:49:02 -0500

    Factura (instalar composer en equipo)

---
+++ factura/print_view.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="es">
+<!--Importación de librerias css y javascript -->
+	<head>
+		<title>Facturación | Hotel Aristo</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="../res/img/famicon.png" />
+		<link rel="stylesheet" type="text/css" href="../css/reporte_factura.css">
+	</head>
+
+    <body>
+        <div>
+            <div class="title">
+                <img alt="Hotel Aristo" src="../res/img/logoA.png">
+            </div>
+            <div class="title_img">
+                <img src="../res/img/titulo_aristo.png">
+            </div>
+            <div class="info_hotel">
+                <p>HENRY QUINTERO</p>
+                <p>NIT. 16.136.920-4</p>
+                <p>RÉGIMEN SIMPLIFICADO</p>
+            </div>
+            <div class="serie">
+                <p class="type"><b>FACTURA DE VENTA</b></p>
+                <p>No&nbsp;&nbsp;&nbsp;&nbsp; C 287</p>
+            </div>
+            
+            <div class="header-right">
+                <p>AVENIDA NORTE N° 45-22</p>
+                <p>CEL. 3213815571</p>
+                <p>TEL. 7469911</p>
+                <p>Email: hotelaristo@hotmail.com</p>
+                <p>TUNJA - BOYACÁ</p>
+            </div>
+            
+            <div class="info">
+                <p><b>Nombre: </b>Juan Eduardo Rodriguez Tobos</p>
+                <p><b>Empresa: </b>Nutresa S.A.  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>Teléfono: </b>7425643</p>
+                <p><b>NIT: </b>890.900.050 – 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Habitación: </b>202 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Fecha Entrada: </b>20/11/2019 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Fecha Salida: </b>25/11/2019</p>
+            </div>
+            
+            <div class="table">
+                <table>
+                    <tr>
+                        <th class="long_col">Descripción</th>
+                        <th>Cantidad</th>
+                        <th>Valor Unitario</th>
+                        <th class="long_value">Valor Total</th>
+                    </tr>
+                    <tr>
+                        <td>Hospedaje habitación ejecutiva</td>
+                        <td>2</td>
+                        <td>$80.000</td>
+                        <td>$160.000</td>
+                    </tr>
+                    <tr>
+                        <td>Minibar</td>
+                        <td>5</td>
+                        <td>$8.000</td>
+                        <td>$16.000</td>
+                    </tr>
+                    <tr>
+                        <td>Servicio de lavandería</td>
+                        <td>4</td>
+                        <td>$3.000</td>
+                        <td>$12.000</td>
+                    </tr>
+                </table>
+            </div>
+            
+            <div class="table_total">
+                <table>
+                    <tr class="long_letter">
+                        <td class="long_total">SON:<br><p>`</p></td>
+                        <td><b>Total $</b></td>
+                        <td class="long_value">$188.000</td>
+                    </tr>
+                </table>
+            </div>
+            
+            <div class="legal"><p>Esta factura se asimila en todos sus efectos legales a una Letra de Cambio. Según el Art. 774 del Código de Comercio</p></div>
+            <div class="rol"><p><b>Responsable:</b> Zuly López</p></div>
+            
+        </div>
+        
+    </body>
+    
+    
+</html>
