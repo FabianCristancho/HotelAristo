0f0a75e1e3e8d65c305646f3492727d26387162a
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2019-12-05 18:14:46 -0500

    Agregado autollenado de datos

--- edicion_cliente/index.php
+++ edicion_cliente/index.php
@@ -87,139 +87,148 @@
         </header>
         
         <div id="informacion-personal-1" class="marco responsive-page">
-					<div class="row-block">
-                        <div class="input-block">
-							<label>Tipo de documento</label>
-							<br>
-							<select class="col-12" id="doc-type-1">
-                                <?php
-                                $array = [
-                                            "CC" => "Cédula de ciudadanía",
-                                            "RC" => "Registro civil",
-                                            "TI" => "Tarjeta de identidad",        
-                                            "CE" => "Cédula de extranjería"];
-                                
-                                echo '<option value="'.$p->getTypeDocument().'" selected>'.$array[$p->getTypeDocument()].'</option>';
-                                printOptions($array[$p->getTypeDocument()], $array);
-                                
-                                function printOptions($value, $array){
-                                    while(list($key, $val)= each($array)){
-                                        if($value!= $val){
-                                            echo '<option value="'.$key.'">'.$val.'</option>';
-                                        }
-                                    }
+            <div class="row-block">
+                <div class="input-block">
+                    <label>Tipo de documento</label>
+                    <br>
+                    <select class="col-12" id="doc-type-1">
+                        <?php
+                        $array = [
+                            "CC" => "Cédula de ciudadanía",
+                            "RC" => "Registro civil",
+                            "TI" => "Tarjeta de identidad",        
+                            "CE" => "Cédula de extranjería"];
+                        echo '<option value="'.$p->getTypeDocument().'" selected>'.$array[$p->getTypeDocument()].'</option>';
+                        printOptions($array[$p->getTypeDocument()], $array);
+                                    
+                        function printOptions($value, $array){
+                            while(list($key, $val)= each($array)){
+                                if($value!= $val){
+                                    echo '<option value="'.$key.'">'.$val.'</option>';
                                 }
-                                ?>
-							</select>
-						</div>
+                            }
+                        }
+                        ?>
+                    </select>
+                </div>
+                
+                <div class="input-block">
+                    <label>Numero de documento</label><br>     
+                    <input id="doc-num-1" class="col-12" type="text" placeholder="Número de documento" pattern="[0-9]{1,15}" value="<?php echo  $p->getNumberDocument(); ?>"</input>
+                </div>
 
-						<div class="input-block">
-							<label>Numero de documento</label>
-							<br>
-                            
-							<input id="doc-num-1" class="col-12" type="text" placeholder="Número de documento" pattern="[0-9]{1,15}" value="<?php echo $p->getNumberDocument(); ?>"</input>
-						</div>
-
-						<div class="input-block">
-							<label>Fecha de expedición</label>
-							<br>
-							<input id="doc-date-1" class="col-12" type="date">
-						</div>
+                <div class="input-block">
+                    <label>Lugar de expedición</label>
+                    <select id="nac-1" class="col-12">
+                        <?php 
+                            echo '<option value="'.$p->getPlaceExpedition().'" selected>'.$p->getPlaceExpedition().'</option>';
+                        ?>
+                    </select>
+                </div>
             </div>
-                        <div class="row-block">
-						<div class="input-block">
-							<label>Nombres</label>
-							<br>
-							<input id="first-1" class="col-12" type="text" placeholder="Nombres" required>
-						</div>
+                
+            <div class="row-block">
+                <div class="input-block">
+                    <label>Nombres</label><br>
+                    <input id="first-1" class="col-12" type="text" placeholder="Nombres" required value="<?php echo $p->getName(); ?>"</input>
+                </div>
 
-						<div class="input-block">
-							<label>Apellidos</label>
-							<br>
-							<input id="last-1" class="col-12" type="text" placeholder="Apellidos" value="Cristacnh" required>
-						</div>
+                <div class="input-block">
+                    <label>Apellidos</label><br>
+                    <input id="last-1" class="col-12" type="text" placeholder="Apellidos" required value="<?php echo $p->getLastName(); ?>"</input>
+                </div>
 
-						<div class="input-block">
-							<label>Telefono</label>
-							<br>
-							<input id="phone-1" class="col-12" type="tel" placeholder="Telefono" pattern="[0-9]{1,15}" required>
-						</div>
+                <div class="input-block">
+                    <label>Telefono</label> <br>
+                    <input id="phone-1" class="col-12" type="tel" placeholder="Telefono" pattern="[0-9]{1,15}" required value="<?php echo $p->getPhone(); ?>"</input>
+                </div>
                             
-
-						<div class="input-block">
-							<label>Correo</label>
-							<br>
-							<input id="email-1" class="col-12" type="email" placeholder="Correo electrónico">
-						</div>
-                            
-                            </div>
+                <div class="input-block">
+                    <label>Correo</label><br>
+                    <input id="email-1" class="col-12" type="email" placeholder="Correo electrónico" value="<?php echo $p->getEmail(); ?>"</input>
+                </div>            
+            </div>
                         
             <div class="row-block">
                 <div class="input-block">
-							<label>Genero</label>
-							<br>
-							<select id="gender-1" class="col-12">
-								<option value="M">Hombre</option>
-								<option value="F">Mujer</option>
-							</select>
-						</div>
+                    <label>Genero</label><br>
+                    <select id="gender-1" class="col-12">
+                        <?php
+                            $array = [
+                                "M" => "Hombre",
+                                "F" => "Mujer"];
+                            echo '<option value="'.$p->getGender().'" selected>'.$array[$p->getGender()].'</option>';
+                            printOptions($array[$p->getGender()], $array);
+                        ?>
+                    </select>
+                </div>
 
-						<div class="input-block">
-							<label>Fecha de nacimiento</label>
-							<br>
-							<input id="birth-1" class="col-12" type="date">
-						</div>
+                <div class="input-block">
+                    <label>Fecha de nacimiento</label><br>
+                    <input id="birth-1" class="col-12" type="date" value="<?php echo $p->getBirthDate(); ?>"</input>
+                </div>
                         
-                        <div class="input-block">
-							<label>Tipo de sangre</label>
-							<br>
-							<select id="blood-1" class="col-12">
-								<option value="O">O</option>
-								<option value="A">A</option>
-								<option value="B">B</option>
-								<option value="AB">AB</option>
-							</select>
-						</div>
+                <div class="input-block">
+                    <label>Tipo de sangre</label><br>
+                    <select id="blood-1" class="col-12">
+                        <?php
+                            $typeBlood = strlen($p->getTypeRH()) > 2 ? substr($p->getTypeRH(), 0, 2):substr($p->getTypeRH(), 0, 1);
+                            $array = [
+                                "O" => "O",
+                                "A" => "A",
+                                "B" => "B",
+                                "AB" => "AB",];
+                            echo '<option value="'.$typeBlood.'" selected>'.$array[$typeBlood].'</option>';
+                            printOptions($array[$typeBlood], $array);
+                        ?>
+                    </select>
+                </div>
 
-						<div class="input-block">
-							<label>RH</label>
-							<br>
-							<select id="rh-1" class="col-12">
-								<option value="+">+ (Positivo)</option>
-								<option value="-">- (Negativo)</option>
-							</select>
-						</div>
+                <div class="input-block">
+                    <label>RH</label><br>
+                    <select id="rh-1" class="col-12">
+                        <?php 
+                            $rh = substr($p->getTypeRH(), -1);
+                            $array = [
+                                "+" => "+ (Positivo)",
+                                "-" => "- (Negativo)"];
+                            echo '<option value="'.$rh.'" selected>'.$array[$rh].'</option>';
+                            printOptions($array[$rh], $array);
+                        ?>
+                    </select>
+                </div>
             
             </div>
                         
-                    <div class="row-block">
-						<div class="input-block">
-							<label>Profesión</label>
-							<br>
-							<select id="profession-1" class="adding-select">
-								<option value="NULL">Ninguna</option>
-								<?php $consult->getList('profession',''); ?>
-							</select>
-							<button>+</button>
-						</div>
+            <div class="row-block">
+                <div class="input-block">
+                    <label>Profesión</label>
+                    <br>
+                    <select id="profession-1" class="adding-select">
+                        <option value="NULL">Ninguna</option>
+                        <?php 
+                            echo '<option value="'.$p->getProfession().'" selected>'.$p->getProfession().'</option>';
+                            $consult->getRemainingProfession($p->getProfession());
+                        ?>
+                    </select>
+                    <button>+</button>
+                </div>
 
-						<div class="input-block">
-							<label>Nacionalidad</label>
-							<br>
-							<select id="nac-1" class="col-12">
-								<?php $consult->getList('country',''); ?>
-							</select>
-						</div>
-                        
-					</div>
-				</div>
+                <div class="input-block">
+                    <label>Nacionalidad</label>
+                    <br>
+                    <select id="nac-1" class="col-12">
+                        <?php 
+                            echo '<option value="'.$p->getPlaceBirth().'" selected>'.$p->getPlaceBirth().'</option>';
+                            $consult->getRemainingNac($p->getPlaceBirth())
+                        ?>
+                    </select>
+                </div>            
+            </div>
+        </div>
+        <a id="button-book" class="col-12">Actualizar Datos</a>
         
-        <?php
-            echo "Cliente con id".$id." se llama ".$p->getName()." ".$p->getLastName()." y nació en ".$p->getPlaceBirth(); 
-        echo $p->getNumberDocument();
-        ?>
         
-
         <div id="aux-footer" class="col-12"></div>
         <footer class="col-12">
             Hotel Aristo 2019
