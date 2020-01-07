e7e507a0df9ac57058d0087b2e9db2b0f1cb0432
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-01-07 11:32:22 -0500

    Act. de rutas y documentación

--- clientes/editar/index.php
+++ clientes/editar/index.php
@@ -1,4 +1,16 @@
 <?php
+    /**
+    * Archivo que contiene la información pertinente a la edición de los clientes almacenados en la base de datos
+    * @package   clientes.editar
+    * @author    Andrés Felipe Chaparro Rosas - Fabian Alejandro Cristancho Rincón
+    * @copyright Todos los derechos reservados. 2020.
+    * @since     Versión 1.0
+    * @version   1.0
+    */
+
+    /**
+    * Incluye la implementación de las clases requeridas para el buen funcionamiento de la aplicación
+    */
     require_once '../../includes/classes.php';
     $consult = new Consult();
 
@@ -9,21 +21,21 @@
     }
 ?>
 
+
 <!DOCTYPE html>
 <html>
-
     <head>
         <title>Clientes | Hotel Aristo</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="../../res/img/famicon.png" />
-        <link rel="stylesheet" type="text/css" href="../../css/main.css">
-        <link rel="stylesheet" type="text/css" href="../../css/main-800.css">
-        <link rel="stylesheet" type="text/css" href="../../css/main-1024.css">
-        <link rel="stylesheet" type="text/css" href="../../css/main-1366.css">
-        <link rel="stylesheet" type="text/css" href="../../css/alerts.css">
-        <script type="text/javascript" src="../../js/moment.js"></script>
-        <script type="text/javascript" src="../../js/dynamic.js"></script>
+        <link rel="shortcut icon" href="/../res/img/famicon.png" />
+        <link rel="stylesheet" type="text/css" href="/../css/main.css">
+        <link rel="stylesheet" type="text/css" href="/../css/main-800.css">
+        <link rel="stylesheet" type="text/css" href="/../css/main-1024.css">
+        <link rel="stylesheet" type="text/css" href="/../css/main-1366.css">
+        <link rel="stylesheet" type="text/css" href="/../css/alerts.css">
+        <script type="text/javascript" src="/../js/moment.js"></script>
+        <script type="text/javascript" src="/../js/dynamic.js"></script>
         <style>
             .row-block{
                 padding-bottom: 10px;
@@ -31,10 +43,19 @@
         </style>
     </head>
     
-    
     <body>
-        <?php include "../../menu/menu.php"; ?>
+
+        <?php 
+            /**
+            * Incluye la implementación de la clase menu, archivo que crea el menú superior de la aplicación web
+            */
+            include "../../menu/menu.php"; 
+        ?>
+        
         <script type="text/javascript">
+             /**
+            * Implementa el método setCurrentPage() pasando como parámetro la cadena de texto "consultar"
+            */
             setCurrentPage("consultar");
         </script>
         
@@ -45,21 +66,29 @@
                     <br>
                     <select class="col-12" id="doc-type-1">
                         <?php
-                        $array = [
-                            "CC" => "Cédula de ciudadanía",
-                            "RC" => "Registro civil",
-                            "TI" => "Tarjeta de identidad",        
-                            "CE" => "Cédula de extranjería"];
-                        echo '<option value="'.$p->getTypeDocument().'" selected>'.$array[$p->getTypeDocument()].'</option>';
-                        printOptions($array[$p->getTypeDocument()], $array);
-                                    
-                        function printOptions($value, $array){
-                            while(list($key, $val)= each($array)){
-                                if($value!= $val){
-                                    echo '<option value="'.$key.'">'.$val.'</option>';
+                            /**
+                            * Crea un array con los tipos de documentos utilizados en la aplicación, con clave y valor
+                            */
+                            $array = [
+                                "CC" => "Cédula de ciudadanía",
+                                "RC" => "Registro civil",
+                                "TI" => "Tarjeta de identidad",        
+                                "CE" => "Cédula de extranjería"];
+                            echo '<option value="'.$p->getTypeDocument().'" selected>'.$array[$p->getTypeDocument()].'</option>';
+                            printOptions($array[$p->getTypeDocument()], $array);
+                            
+                            /**
+                            * Función que se encarga de imprimir los valores referentes al valor que se pasa por parámetro
+                            * @param String $value Valor del array
+                            * @param Array $array Array con una serie de valores que dependen del valor del mismo
+                            */
+                            function printOptions($value, $array){
+                                while(list($key, $val)= each($array)){
+                                    if($value!= $val){
+                                        echo '<option value="'.$key.'">'.$val.'</option>';
+                                    }
                                 }
                             }
-                        }
                         ?>
                     </select>
                 </div>
@@ -106,6 +135,9 @@
                     <label>Genero</label><br>
                     <select id="gender-1" class="col-12">
                         <?php
+                            /**
+                            * Crea un array con los géneros y le asigna a la persona su respectivo valor
+                            */
                             $array = [
                                 "M" => "Hombre",
                                 "F" => "Mujer"];
@@ -124,6 +156,9 @@
                     <label>Tipo de sangre</label><br>
                     <select id="blood-1" class="col-12">
                         <?php
+                            /**
+                            * Crea un array con los tipos de sangre y asigna el valor correspondiente a la persona que se está editando
+                            **/
                             $typeBlood = strlen($p->getTypeRH()) > 2 ? substr($p->getTypeRH(), 0, 2):substr($p->getTypeRH(), 0, 1);
                             $array = [
                                 "O" => "O",
@@ -139,7 +174,10 @@
                 <div class="input-block">
                     <label>RH</label><br>
                     <select id="rh-1" class="col-12">
-                        <?php 
+                        <?php
+                            /*+
+                            * Extrae de la base de datos el rh de la persona, y a partir de este crea los valores para los demás rh 
+                            */
                             $rh = substr($p->getTypeRH(), -1);
                             $array = [
                                 "+" => "+ (Positivo)",
@@ -158,7 +196,10 @@
                     <br>
                     <select id="profession-1" class="adding-select">
                         <option value="NULL">Ninguna</option>
-                        <?php 
+                        <?php
+                            /**
+                            * Obtiene la profesión de la persona a la cual se está editando
+                            */
                             echo '<option value="'.$p->getProfession().'" selected>'.$p->getProfession().'</option>';
                             $consult->getRemainingProfession($p->getProfession());
                         ?>
@@ -170,7 +211,10 @@
                     <label>Nacionalidad</label>
                     <br>
                     <select id="nac-1" class="col-12">
-                        <?php 
+                        <?php
+                            /**
+                            * Obtiene el lugar de nacimiento de la persona que se está editando
+                            **/
                             echo '<option value="'.$p->getPlaceBirth().'" selected>'.$p->getPlaceBirth().'</option>';
                             $consult->getRemainingNac($p->getPlaceBirth())
                         ?>
@@ -180,10 +224,12 @@
         </div>
         <a id="button-book" class="col-12">Actualizar Datos</a>
         
+        <?php
+            /**
+            * Incluye la implementación del archivo que contiene el footer con la información de la aplicación web
+            */
+            include "../../footer/footer.php"; 
+        ?>
         
-        <div id="aux-footer" class="col-12"></div>
-        <footer class="col-12">
-            Hotel Aristo 2019
-        </footer>
     </body>
 </html>
