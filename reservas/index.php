ad236b6413ef1d33bd7df9d5c523e777f3f34cc0
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-01-06 20:30:32 -0500

    Actualización de rutas

--- reservas/index.php
+++ reservas/index.php
@@ -6,32 +6,74 @@
   <title>Reservas | Hotel Aristo</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="/res/img/famicon.png" />
-  <link rel="stylesheet" type="text/css" href="/css/main.css">
-  <link rel="stylesheet" type="text/css" href="/css/main-800.css">
-  <link rel="stylesheet" type="text/css" href="/css/main-1024.css">
-  <link rel="stylesheet" type="text/css" href="/css/main-1220.css">
-  <link rel="stylesheet" type="text/css" href="/css/main-1366.css">
-  <link rel="stylesheet" type="text/css" href="/css/alerts.css">
-  <link rel="stylesheet" type="text/css" href="/css/modal.css">
-  <script type="text/javascript" src="/js/moment.js"></script>
-  <script type="text/javascript" src="/js/dynamic.js"></script>
+  <link rel="shortcut icon" href="../res/img/famicon.png" />
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <link rel="stylesheet" type="text/css" href="../css/main-800.css">
+  <link rel="stylesheet" type="text/css" href="../css/main-1024.css">
+  <link rel="stylesheet" type="text/css" href="../css/main-1220.css">
+  <link rel="stylesheet" type="text/css" href="../css/main-1366.css">
+  <link rel="stylesheet" type="text/css" href="../css/alerts.css">
+  <link rel="stylesheet" type="text/css" href="../css/modal.css">
+  <script type="text/javascript" src="../js/moment.js"></script>
+  <script type="text/javascript" src="../js/dynamic.js"></script>
 </head>
 
 <body>
 <!--Menu de la aplicación web del hotel Aristo la clase main-menu-item pertenece a los botones del menú-->
-     <?php
-        include "../menu/menu.php"; 
-    ?>
-    <script type="text/javascript">
-        setCurrentPage("consultar");
-    </script>
+  <header class="col-12">
+      <a href="../inicio">
+        <img id="logo-hotel" src="../res/img/logoA.png">
+      </a>
+      <button  onclick="window.location.href = '/inicio';" class="main-menu-item menu-item" >
+        <img src="../res/img/home-icon-black.png">
+        <p>Inicio</p>
+      </button>
+
+      <div class="dropdown menu-item">
+        <button onclick="window.location.href = '';"   class="main-menu-item">
+          <img src="../res/img/book-icon-black.png">
+          <p>Registrar</p>
+        </button>
+        <br>
+        <div class="dropdown-content">
+          <a href="../nueva_reserva">Registar reserva</a>
+          <a href="../nueva_empresa">Registrar empresas</a>
+        </div>
+      </div>
+
+      <div class="dropdown menu-item">
+        <button id="current-item" onclick="window.location.href = '';" class="main-menu-item">
+          <img src="../res/img/book-icon-white.png">
+          <p>Consultar</p>
+        </button>
+        <br>
+        <div class="dropdown-content">
+          <a href="../reservas">Consultar reservas</a>
+          <a href="../clientes">Consultar clientes</a>
+          <a href="../empresas">Consultar empresas</a>
+          <a href="../habitaciones">Consultar habitaciones</a>
+        </div>
+      </div>
+
+      <button onclick="window.location.href = '../control_diario';" class="main-menu-item menu-item">
+        <img src="../res/img/control-icon-black.png">
+        <p>Control diario</p>
+      </button>
+      <button onclick="window.location.href = '../facturas';" class="main-menu-item menu-item">
+        <img src="../res/img/bill-icon-black.png">
+        <p>Facturación</p>
+      </button>
+      <button onclick="window.location.href = '../includes/logout.php';" class="main-menu-item menu-item">
+        <img src="../res/img/logout-icon-black.png">
+        <p>Cerrar sesión</p>
+      </button>
+    </header>
 
 
     <div id="content" class="col-12">
         <div class="marco nearly-page">
             <h3 id="requests-title" class="col-9">Reservas de huéspedes</h3>
-            <a href="registrar" id="button-add-book" class="col-2" href="">Nueva reserva</a>
+            <a id="button-add-book" class="col-2" href="">Nueva reserva</a>
             
             <table>
                 <tr>
@@ -51,7 +93,7 @@
     </div>
     <div id="aux-footer" class="col-12"></div>
     <footer>
-        <a href="/home/index.php" class="info">Hotel Aristo</a> &copy; 2019 | Todos los derechos reservados
+        <a href="../inicio/" class="info">Hotel Aristo</a> &copy; 2019 | Todos los derechos reservados
     </footer>
 </body>
 </html>
