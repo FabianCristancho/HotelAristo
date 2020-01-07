5668fadb2d6eed471f86b7e7fa2643fbf71e6a73
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-01-07 14:39:27 -0500

    Rutas y documentación

--- habitaciones/detalles/index.php
+++ habitaciones/detalles/index.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <title>Historial de Habitación | Hotel Aristo</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="../../res/img/famicon.png" />
-        <link rel="stylesheet" type="text/css" href="../../css/main.css">
-        <link rel="stylesheet" type="text/css" href="../../css/main-800.css">
-        <link rel="stylesheet" type="text/css" href="../../css/main-1024.css">
-        <link rel="stylesheet" type="text/css" href="../../css/main-1220.css">
-        <link rel="stylesheet" type="text/css" href="../../css/main-1366.css">
-        <link rel="stylesheet" type="text/css" href="../../css/alerts.css">
-        <script type="text/javascript" src="../../js/moment.js"></script>
-        <script type="text/javascript" src="../../js/dynamic.js"></script>
+        <link rel="shortcut icon" href="/../res/img/famicon.png" />
+        <link rel="stylesheet" type="text/css" href="/../css/main.css">
+        <link rel="stylesheet" type="text/css" href="/../css/main-800.css">
+        <link rel="stylesheet" type="text/css" href="/../css/main-1024.css">
+        <link rel="stylesheet" type="text/css" href="/../css/main-1220.css">
+        <link rel="stylesheet" type="text/css" href="/../css/main-1366.css">
+        <link rel="stylesheet" type="text/css" href="/../css/alerts.css">
+        <script type="text/javascript" src="/../js/moment.js"></script>
+        <script type="text/javascript" src="/../js/dynamic.js"></script>
     </head>
 
     <body onload ="getDate('control-date',0);">
-            <?php include "../../menu/menu.php"; ?>
-            <script type="text/javascript">
-                setCurrentPage("control-diario");
-            </script>
+        <?php 
+            /**
+            * Incluye la implementación de la clase menu, archivo que crea el menú superior de la aplicación web
+            */
+            include "../../menu/menu.php"; 
+        ?>
+        
+        <script type="text/javascript">
+            /**
+            * Implementa el método setCurrentPage() pasando como parámetro la cadena de texto "control-diario"
+            */
+            setCurrentPage("control-diario");
+        </script>
 
         <div id="content" class="col-12">
             <div class="marco nearly-page">
@@ -46,9 +54,9 @@
                         <label><b>Valor total de consumo ($): &ensp;</b></label>
                         <label>75.000</label>
                     </div>
-
                 </div>
                 </br>
+            
                 <div class="specific-info">
                     <div>
                         <h4><b>HUÉSPEDES</b></h4>
@@ -98,13 +106,16 @@
             </div>
 
             <div class="button-return">
-                <a href="../../habitaciones/index.php">Regresar</a>
+                <a href="/../habitaciones/">Regresar</a>
             </div>
         </div>
-
-        <div id="aux-footer" class="col-12"></div>
-        <footer>
-            <a href="../../inicio/" class="info">Hotel Aristo</a> &copy; 2019 | Todos los derechos reservados
-        </footer>
+        
+        <?php
+            /**
+            * Incluye la implementación del archivo que contiene el footer con la información de la aplicación web
+            */
+            include "../../footer/footer.php"; 
+        ?>
+    
     </body>
 </html>
