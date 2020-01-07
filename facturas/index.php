1dfb984672c2bf92d0bf41e1e9911664542c7cf6
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-01-07 14:38:37 -0500

    Rutas y documentación

--- facturas/index.php
+++ facturas/index.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <title>Facturas | Hotel Aristo</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="../res/img/famicon.png" />
-        <link rel="stylesheet" type="text/css" href="../css/main.css">
-        <link rel="stylesheet" type="text/css" href="../css/main-800.css">
-        <link rel="stylesheet" type="text/css" href="../css/main-1024.css">
-        <link rel="stylesheet" type="text/css" href="../css/main-1366.css">
-        <link rel="stylesheet" type="text/css" href="../css/alerts.css">
-        <script type="text/javascript" src="../js/moment.js"></script>
-        <script type="text/javascript" src="../js/dynamic.js"></script>
+        <link rel="shortcut icon" href="/res/img/famicon.png" />
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <link rel="stylesheet" type="text/css" href="/css/main-800.css">
+        <link rel="stylesheet" type="text/css" href="/css/main-1024.css">
+        <link rel="stylesheet" type="text/css" href="/css/main-1366.css">
+        <link rel="stylesheet" type="text/css" href="/css/alerts.css">
+        <script type="text/javascript" src="/js/moment.js"></script>
+        <script type="text/javascript" src="/js/dynamic.js"></script>
     </head>
 
     <body>
-        <?php include "../menu/menu.php"; ?>
+        <?php
+            /**
+            * Incluye la implementación de la clase menu, archivo que crea el menú superior de la aplicación web
+            */
+            include "../menu/menu.php"; 
+        ?>
+        
         <script type="text/javascript">
+            /**
+            * Implementa el método setCurrentPage() pasando como parámetro la cadena de texto "facturas"
+            */
             setCurrentPage("facturas");
         </script>
 
         <div id="content" class="col-12">
-
             <div class="marco nearly-page">
                 <h1 class="heading">FACTURAS (Registro es un ejemplo para enlazar reportes)</h1>
                 <a href="factura" id="button-more-info" class="btn-new-bill">NUEVA FACTURA</a>
@@ -42,9 +49,13 @@
                 </div>
             </div>
         </div>
-        <div id="aux-footer" class="col-12"></div>
-        <footer class="col-12">
-            Hotel Aristo 2019
-        </footer>
+        
+        <?php
+            /**
+            * Incluye la implementación del archivo que contiene el footer con la información de la aplicación web
+            */
+            include "../footer/footer.php"; 
+        ?>
+        
     </body>
 </html>
