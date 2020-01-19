af46e44faa0079da10d3b7c318d45ccab93d139a
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-01-19 18:14:20 -0500

    Generación de reportes

---
+++ reportes/report.php
@@ -0,0 +1,58 @@
+<?php
+    /**
+    * Clase Report. Contiene la plantilla utilizada para el hotel, con el fin de dar un formato establecido a los reportes que se requieren 
+    * @package   reportes
+    * @author    Andrés Felipe Chaparro Rosas - Fabian Alejandro Cristancho Rincón
+    * @copyright Todos los derechos reservados. 2020.
+    * @since     Versión 1.0
+    * @version   1.0
+    */
+
+    /**
+    * Incluye la implementación del archivo fpdf necesario para la exitosa generación de reportes
+    */
+    require('fpdf.php');
+
+    class Report extends FPDF {
+
+        /**
+        * Función que contiene el formato de cabecera del reporte
+        */
+        function Header($title)
+        {
+            // Logo
+            $this->Image('../../res/img/logoA.png',10,8,20);
+            $this->Image('../../res/img/titulo_aristo.png',35,13,35);
+            // Arial bold 18
+            $this->SetFont('Arial','B',18);
+            // Movernos a la derecha
+            $this->Cell(55);
+            // Título
+            $this->Cell(100,70,$title,0,0,'C');
+            $this->SetFont('Arial','',8);
+            // Movernos a la derecha
+            $this->Cell(50);
+            // Información del hotel
+            $this->Cell(0,2,utf8_decode('AVENIDA NORTE N° 45-22'),0,0,'R');
+            $this->Cell(0,10,utf8_decode('CEL. 3213815571'),0,0,'R');
+            $this->Cell(0,18,utf8_decode('TEL. 7469911'),0,0,'R');
+            $this->Cell(0,26,utf8_decode('Email: hotelaristo@hotmail.com'),0,0,'R');
+            $this->Cell(0,34,utf8_decode('TUNJA - BOYACÁ'),0,0,'R');
+            // Salto de línea
+            $this->Ln(20);
+        }
+
+        /**
+        * Función que contiene el formato de salto de página del reporte
+        */
+        function Footer()
+        {
+            $this->SetY(-15);
+            // Arial italic 8
+            $this->SetFont('Arial','I',8);
+            // Número de página
+            $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C');
+            $this->AliasNbPages();
+        }
+    }
+?>
