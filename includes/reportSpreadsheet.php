9e2f243ec5a25a2b9080a374074831e408d43041
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-01-30 23:05:44 -0500

    Reporte excel V1

--- includes/reportSpreadsheet.php
+++ includes/reportSpreadsheet.php
@@ -1,10 +1,22 @@
 <?php
     use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
     use PhpOffice\PhpSpreadsheet\Shared\Date;
+    use PhpOffice\PhpSpreadsheet\Style\Border;
+    use PhpOffice\PhpSpreadsheet\Style\Fill;
+
     class ReportSpreadsheet extends Database2{
         
+        function createBorder($rangeCell, $spreadsheet){
+            $spreadsheet->getActiveSheet()->getStyle($rangeCell)->getBorders()->getAllBorders()->applyFromArray( array( 'borderStyle' => Border::BORDER_THIN, 'color' => array( 'rgb' => '000000' ) ) );
+        }
         
-         function fullRoom($numberCol, $numberRow, $firstRoom, $cantRooms, $spreadsheet){
+        
+        function setBackgroundColor($rangeCell, $color, $spreadsheet){
+            $spreadsheet->getActiveSheet()->getStyle($rangeCell)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB($color);
+        }
+        
+        
+        function fullRoom($numberCol, $numberRow, $firstRoom, $cantRooms, $spreadsheet){
             $box = ord($numberCol);
             $room = $firstRoom;
     
@@ -22,6 +34,7 @@
             }
         }
         
+        
         function getCountSale($col, $row, $iterations, $spreadsheet){
             $box = ord($col);
             $aux = '';
@@ -38,6 +51,7 @@
                 $box++;
             }
         }
+        
         
         function getSalePerRoom($firstCol, $row, $spreadsheet){
             $query = $this->connect()->prepare('SELECT numero_habitacion, SUM(valor_ocupacion*(fecha_salida-fecha_ingreso+1)) AS value 
@@ -72,213 +86,128 @@
         }
         
         
-        
-        
-        
-        function fullValuesPerRoom($room, $col, $spreadsheet, $month, $year){
-            $numberDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-            $day = 1;
+        function getSalePerDay($col, $cantDays, $spreadsheet){
             $row = 3;
-            $colExcelDate = 2;
-            $rowExcelDate = 3;
-            
-            $query = $this->connect()->prepare('SELECT valor_ocupacion, DATE_FORMAT(fecha_ingreso, "%d/%m/%Y") AS fi, DATE_FORMAT(fecha_salida, "%d/%m/%Y") AS fs, SUM(fecha_salida - fecha_ingreso + 1) AS dif 
-            FROM tarifas t INNER JOIN registros_habitacion rh ON t.id_tarifa=rh.id_tarifa
-            INNER JOIN reservas r ON r.id_reserva=rh.id_reserva
-            INNER JOIN habitaciones h ON h.id_habitacion=rh.id_habitacion
-            WHERE h.numero_habitacion = :room
-            GROUP BY 1, 2, 3
-            ORDER BY fi, fs');
-            $query->execute(['room'=>$room]);
-            
-            $dateTimeNow = strtotime((string)($day).'/'.$month.'/'.$year.'+ 1 days');
-
-            foreach ($query as $current) {
-                
-                //$val = $spreadsheet->getActiveSheet()->getCellByColumnAndRow($colExcelDate, $rowExcelDate)->getValue();
-                //$aux = Date::excelToTimestamp($val); //formato unix timestamp
-                
-                $diferenceDates = $current['dif'];
-                
-                //$fecha_de_excel = date('d/m/Y', $aux); //string
-                //$fecha_ingreso_bd = date_create($current['fi']);
-                //$fecha_de_excel = $aux; //string
-                //$newDate = date("d/m/Y", strtotime($current['fi']));
-                $dateString = date('d/m/Y', $dateTimeNow);
-                
-                while(strcmp ($current['fi'] , $dateString )!=0 && $day<$numberDays){
-                    $day++;
-                    $dateTimeNow = strtotime((string)($day).'/'.$month.'/'.$year.'+ 1 days');
-                }
-                
-                $dateString = date('d/m/Y', $dateTimeNow);
-                if(strcmp ($current['fi'] , $dateString )==0){
-                    for($i=0;$i<$diferenceDates;$i++){
-                        $spreadsheet->getActiveSheet()->setCellValue($col.$row, $current['valor_ocupacion']);
-                        $row++;
-                    }
-                }else{
-                    $spreadsheet->getActiveSheet()->setCellValue('E5', $dateString);
-                }
-                
-                
-                /*for($i=0;$i<$numberDays;$i++){
-                    $dateTimeNow = strtotime((string)($day).'-'.$month.'-'.$year.'+ 1 days');
-                    $spreadsheet->getActiveSheet()->getStyle('B'.$row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
-                    $spreadsheet->getActiveSheet()->setCellValue('B'.$row, Date::PHPToExcel($dateTimeNow)); 
-                    $day++;
-                    $row++;
-                } */
-                
-                
-                
-                //$fecha_ingreso_bd = strtotime($newDate."+ 1 days");
-                //$cad = (string)($current['fi']);
-                //$dateTimeNow = date('d/m/Y', strtotime($cad));
-                //$spreadsheet->getActiveSheet()->getStyle('E5')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
-                //$spreadsheet->getActiveSheet()->setCellValue('E5', Date::PHPToExcel($dateTimeNow)); 
-                //$spreadsheet->getActiveSheet()->setCellValue('E4', $current['fi']); 
-                //$spreadsheet->getActiveSheet()->getStyle('E6')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
-                //$cadena_nueva = date_format($fecha_de_excel, 'd/m/Y');
-                 //$spreadsheet->getActiveSheet()->setCellValue('C4', $fecha_de_excel); 
-                //$spreadsheet->getActiveSheet()->setCellValue('E6', $fecha_de_excel);
-                
-                
-               // $spreadsheet->getActiveSheet()->getStyle('E5')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_YYYYMMDD2);
-                //$spreadsheet->getActiveSheet()->setCellValue('E5', Date::timestampToExcel($fecha_ingreso_bd));
-                //echo 'Excel vale: '.$fecha_de_excel;
-                //echo 'Bases vale: '.$cad;
-                //echo 'Bases es: '.$current['fi'].'|||||||||';
-                //echo $fecha_de_excel+$fecha_ingreso_bd;
-                //$interval = $fecha_de_excel->diff($fecha_ingreso_bd);
-                //echo 'Fecha es '.$current['fi'];
-                //$result = $fecha_ingreso_bd->format('d/m/Y');
-                //echo $result;
-               //$interval = $aux2->format('D');
-                
-                /*if(strcmp ($current['fi'] , $fecha_de_excel )==0){
-                    for($i=0;$i<$diferenceDates;$i++){
-                        //echo 'En la fila '.$row.' hay resultados';
-                        $spreadsheet->getActiveSheet()->setCellValue($col.$row, $current['valor_ocupacion']);
-                        $row++;
-                        $rowExcelDate++;
-                    }
-                }else{
-                    //echo 'En la fila '.$row.' NOO hay resultados';
-                    $row++;
-                    $rowExcelDate++;
-                } */
+            for($i=0;$i<$cantDays;$i++){
+                $spreadsheet->getActiveSheet()->setCellValue($col.$row, '=SUM(C'.$row.':S'.$row.')');
+                $row++;
             }
         }
         
         
-        
-        
-        
-       /* function fullValuesPerRoom($room, $col, $spreadsheet){
-            $row = 4;
-            $colExcelDate = 2;
-            $rowExcelDate = 3;
-            
-            $query = $this->connect()->prepare('SELECT valor_ocupacion, DATE_FORMAT(fecha_ingreso, "%d/%m/%Y") AS fi, DATE_FORMAT(fecha_salida, "%d/%m/%Y") AS fs, SUM(fecha_salida - fecha_ingreso + 1) AS dif 
-            FROM tarifas t INNER JOIN registros_habitacion rh ON t.id_tarifa=rh.id_tarifa
-            INNER JOIN reservas r ON r.id_reserva=rh.id_reserva
-            INNER JOIN habitaciones h ON h.id_habitacion=rh.id_habitacion
-            WHERE h.numero_habitacion = :room
-            GROUP BY 1, 2, 3
-            ORDER BY fi, fs');
-            $query->execute(['room'=>$room]);
+        function setColorBgToValue($paymentMethod){
+            $color = 'ffffff';
+            switch ($paymentMethod) {
+                case 'E':
+                    $color = '9bc2e6';
+                break;
 
-            foreach ($query as $current) {
-                
-                $val = $spreadsheet->getActiveSheet()->getCellByColumnAndRow($colExcelDate, $rowExcelDate)->getValue();
-                $aux = Date::excelToTimestamp($val); //formato unix timestamp
-                
-                $diferenceDates = $current['dif'];
-                
-                $fecha_de_excel = date('d/m/Y', $aux); //string
-                //$fecha_ingreso_bd = date_create($current['fi']);
-                //$fecha_de_excel = $aux; //string
-                //$newDate = date("d/m/Y", strtotime($current['fi']));
-                
-                
-                
-                //$fecha_ingreso_bd = strtotime($newDate."+ 1 days");
-                //$cad = (string)($current['fi']);
-                //$dateTimeNow = date('d/m/Y', strtotime($cad));
-                //$spreadsheet->getActiveSheet()->getStyle('E5')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
-                //$spreadsheet->getActiveSheet()->setCellValue('E5', Date::PHPToExcel($dateTimeNow)); 
-                $spreadsheet->getActiveSheet()->setCellValue('E4', $current['fi']); 
-                //$spreadsheet->getActiveSheet()->getStyle('E6')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
-                //$cadena_nueva = date_format($fecha_de_excel, 'd/m/Y');
-                 $spreadsheet->getActiveSheet()->setCellValue('C4', $fecha_de_excel); 
-                //$spreadsheet->getActiveSheet()->setCellValue('E6', $fecha_de_excel);
-                
-                
-               // $spreadsheet->getActiveSheet()->getStyle('E5')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_YYYYMMDD2);
-                //$spreadsheet->getActiveSheet()->setCellValue('E5', Date::timestampToExcel($fecha_ingreso_bd));
-                //echo 'Excel vale: '.$fecha_de_excel;
-                //echo 'Bases vale: '.$cad;
-                //echo 'Bases es: '.$current['fi'].'|||||||||';
-                //echo $fecha_de_excel+$fecha_ingreso_bd;
-                //$interval = $fecha_de_excel->diff($fecha_ingreso_bd);
-                //echo 'Fecha es '.$current['fi'];
-                //$result = $fecha_ingreso_bd->format('d/m/Y');
-                //echo $result;
-               //$interval = $aux2->format('D');
-                
-                if(strcmp ($current['fi'] , $fecha_de_excel )==0){
-                    for($i=0;$i<$diferenceDates;$i++){
-                        //echo 'En la fila '.$row.' hay resultados';
-                        $spreadsheet->getActiveSheet()->setCellValue($col.$row, $current['valor_ocupacion']);
-                        $row++;
-                        $rowExcelDate++;
-                    }
-                }else{
-                    //echo 'En la fila '.$row.' NOO hay resultados';
-                    $row++;
-                    $rowExcelDate++;
-                } 
+                case 'T':
+                    $color = '00ff00';
+                break;
+
+                case 'C':
+                    $color = 'ffc000';
+                break;
+
+                case 'CC':
+                    $color = 'ff33cc';
+                break;
+
             }
-        }*/
+            return $color;
+        }
         
-        /*function fullValuesPerRoom($room, $col, $spreadsheet){
+        
+        public function fullValuesPerRoom2($room, $col, $spreadsheet){
+            
             $row = 3;
             $colExcelDate = 2;
             $rowExcelDate = 3;
             
-            $query = $this->connect()->prepare('SELECT valor_ocupacion, DATE_FORMAT(fecha_ingreso, "%d/%m/%Y") AS fi, DATE_FORMAT(fecha_salida, "%d/%m/%Y") AS fs, SUM(fecha_salida - fecha_ingreso + 1) AS dif 
+            $query = $this->connect()->prepare('SELECT valor_ocupacion, DAY (fecha_ingreso) AS dia_ingreso, SUM(fecha_salida - fecha_ingreso + 1) AS cantidad_dias, medio_pago 
             FROM tarifas t INNER JOIN registros_habitacion rh ON t.id_tarifa=rh.id_tarifa
             INNER JOIN reservas r ON r.id_reserva=rh.id_reserva
             INNER JOIN habitaciones h ON h.id_habitacion=rh.id_habitacion
             WHERE h.numero_habitacion = :room
-            GROUP BY 1, 2, 3
-            ORDER BY fi, fs');
+            GROUP BY 1, 2
+            ORDER BY fecha_ingreso');
             $query->execute(['room'=>$room]);
+            
 
             foreach ($query as $current) {
+                $entryDay = $current['dia_ingreso'];
+                $diferenceDates = $current['cantidad_dias'];
+                $valueRaservation = $current['valor_ocupacion'];
+                $color = $this->setColorBgToValue($current['medio_pago']);
                 
-                $val = $spreadsheet->getActiveSheet()->getCellByColumnAndRow($colExcelDate, $rowExcelDate)->getValue();
-                $aux = Date::excelToTimestamp($val); //formato unix timestamp
-                
-                $diferenceDates = $current['dif'];
-                
-                $fecha_de_excel = date_create(date('d/m/Y', $aux)); 
-                $fecha_ingreso_bd = date_create($current['fi']);
-                
-                $aux2 = date_diff($fecha_de_excel,$fecha_ingreso_bd);
-                $interval = $aux2->format('D');
-                
-                if($interval == 0){
-                    for($i=0;$i<$diferenceDates;$i++){
-                        $spreadsheet->getActiveSheet()->setCellValue($col.$row, $current['valor_ocupacion']);
-                        $row++;
-                    }
-                }else{
-                    $row++;
+                for($i=0;$i<$diferenceDates;$i++){
+                    $this->setBackgroundColor($col.($entryDay+2), $color, $spreadsheet);
+                    $spreadsheet->getActiveSheet()->setCellValue($col.($entryDay+2), $valueRaservation);
+                    $entryDay++;
                 }
+                
             }
-        }*/
+        }
+        
+        
+        function getTotalPaymentMethod($paymentMethod, $spreadsheet){
+            $totalPaymentMethod = 0;
+            $query = $this->connect()->prepare('SELECT SUM(valor_ocupacion*(fecha_salida-fecha_ingreso+1)) AS total
+            FROM tarifas t INNER JOIN registros_habitacion rh ON t.id_tarifa=rh.id_tarifa
+            INNER JOIN reservas r ON r.id_reserva=rh.id_reserva
+            WHERE r.medio_pago = :medio');
+            $query->execute(['medio'=>$paymentMethod]);
+            foreach ($query as $current) {
+                $totalPaymentMethod += $current['total'];
+            }
+            return $totalPaymentMethod;
+        }
+        
+        
+        function getValueTotalPerRoom($initCol, $cantRooms, $spreadsheet){
+            $colAux = ord($initCol);
+            for($i=0;$i<$cantRooms;$i++){
+                $spreadsheet->getActivesheet()->setCellValue(chr($colAux).'34', '=SUM('.chr($colAux).'3:'.chr($colAux).'33)');  
+                $colAux++;
+            }
+        }
+        
+        
+        function getTotalCountPerRoom($initCol, $cantRooms, $spreadsheet){
+            $colAux = ord($initCol);
+            for($i=0;$i<$cantRooms;$i++){
+                $spreadsheet->getActivesheet()->setCellValue(chr($colAux).'35', '=COUNT('.chr($colAux).'3:'.chr($colAux).'33)');  
+                $colAux++;
+            }
+        }
+        
+        
+        function fullAllRooms($numberCol, $spreadsheet){
+            $box = ord($numberCol);
+            $room = 201;
+    
+            for ($i = 0; $i < 17; $i++) {
+                $this->fullValuesPerRoom2($room, chr($box), $spreadsheet);
+                if($room==202){
+                    $room=301;
+                }else if(($room==304)||($room==404)||($room==504)){
+                    $room+=97;
+                }else{
+                    $room++;    
+                }
+                
+                $box++;
+            }
+        }
+        
+        
+        function changeDollarFormat($cell, $spreadsheet){
+            $spreadsheet->getActiveSheet()
+            ->getStyle($cell)
+            ->getNumberFormat()
+            ->setFormatCode(NumberFormat::FORMAT_CURRENCY_USD);
+        }
         
         
         function extractDateFromExcel($numberColExtract, $numberRowExtract, $spreadsheet){
