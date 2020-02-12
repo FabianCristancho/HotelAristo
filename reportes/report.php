eb56e528c53e86bcffe4e5a41cdc08426f7fa52b
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-02-12 00:30:56 -0500

    Módulo Nueva Factura Listo

--- reportes/report.php
+++ reportes/report.php
@@ -21,8 +21,8 @@
         function Header($title)
         {
             // Logo
-            $this->Image('../../res/img/logoA.png',5,6,23);
-            $this->Image('../../res/img/titulo_aristo.png',32,9,44);
+            $this->Image('../../res/img/logoA.png',10,6,23);
+            $this->Image('../../res/img/titulo_aristo.png',37,9,44);
             // Arial bold 18
             $this->SetFont('Arial','B',18);
             // Movernos a la derecha
@@ -48,6 +48,63 @@
             // Salto de línea
             //$this->Ln(20);
         }
+        
+        function RoundedRect($x, $y, $w, $h, $r, $style = '', $angle = '1234')
+    {
+        $k = $this->k;
+        $hp = $this->h;
+        if($style=='F')
+            $op='f';
+        elseif($style=='FD' or $style=='DF')
+            $op='B';
+        else
+            $op='S';
+        $MyArc = 4/3 * (sqrt(2) - 1);
+        $this->_out(sprintf('%.2f %.2f m', ($x+$r)*$k, ($hp-$y)*$k ));
+ 
+        $xc = $x+$w-$r;
+        $yc = $y+$r;
+        $this->_out(sprintf('%.2f %.2f l', $xc*$k, ($hp-$y)*$k ));
+        if (strpos($angle, '2')===false)
+            $this->_out(sprintf('%.2f %.2f l', ($x+$w)*$k, ($hp-$y)*$k ));
+        else
+            $this->_Arc($xc + $r*$MyArc, $yc - $r, $xc + $r, $yc - $r*$MyArc, $xc + $r, $yc);
+ 
+        $xc = $x+$w-$r;
+        $yc = $y+$h-$r;
+        $this->_out(sprintf('%.2f %.2f l', ($x+$w)*$k, ($hp-$yc)*$k));
+        if (strpos($angle, '3')===false)
+            $this->_out(sprintf('%.2f %.2f l', ($x+$w)*$k, ($hp-($y+$h))*$k));
+        else
+            $this->_Arc($xc + $r, $yc + $r*$MyArc, $xc + $r*$MyArc, $yc + $r, $xc, $yc + $r);
+ 
+        $xc = $x+$r;
+        $yc = $y+$h-$r;
+        $this->_out(sprintf('%.2f %.2f l', $xc*$k, ($hp-($y+$h))*$k));
+        if (strpos($angle, '4')===false)
+            $this->_out(sprintf('%.2f %.2f l', ($x)*$k, ($hp-($y+$h))*$k));
+        else
+            $this->_Arc($xc - $r*$MyArc, $yc + $r, $xc - $r, $yc + $r*$MyArc, $xc - $r, $yc);
+ 
+        $xc = $x+$r ;
+        $yc = $y+$r;
+        $this->_out(sprintf('%.2f %.2f l', ($x)*$k, ($hp-$yc)*$k ));
+        if (strpos($angle, '1')===false)
+        {
+            $this->_out(sprintf('%.2f %.2f l', ($x)*$k, ($hp-$y)*$k ));
+            $this->_out(sprintf('%.2f %.2f l', ($x+$r)*$k, ($hp-$y)*$k ));
+        }
+        else
+            $this->_Arc($xc - $r, $yc - $r*$MyArc, $xc - $r*$MyArc, $yc - $r, $xc, $yc - $r);
+        $this->_out($op);
+    }
+ 
+    function _Arc($x1, $y1, $x2, $y2, $x3, $y3)
+    {
+        $h = $this->h;
+        $this->_out(sprintf('%.2f %.2f %.2f %.2f %.2f %.2f c ', $x1*$this->k, ($h-$y1)*$this->k,
+            $x2*$this->k, ($h-$y2)*$this->k, $x3*$this->k, ($h-$y3)*$this->k));
+    }
 
         /**
         * Función que contiene el formato de salto de página del reporte
